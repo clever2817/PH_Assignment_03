111fe1bbee81f6aaa97a696ea0de1bbf6353e63b
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-08-04 21:54:03 +0600

    Problem 3 solved

--- assignment3.js
+++ assignment3.js
@@ -21,3 +21,20 @@ else
 {
     console.log("Chocolate");
 }
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11 ;
+//write your code here
+for(let i=1; i<=lastDay; i++)
+{
+    if(i % 3 === 0)
+    {
+        console.log(i + " - " + "medicine");
+    }
+
+    else
+    {
+        console.log(i + " - " + "rest");
+    }
+}
