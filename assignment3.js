483b1276f178600dd1d05584af31c8ea689d4ef6
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-08-04 21:45:20 +0600

    Problem 2 solved

--- assignment3.js
+++ assignment3.js
@@ -2,3 +2,22 @@
 var area = 800;
 //write your code here
 console.log(area / 2);
+
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if(money >= 25000)
+{
+    console.log("Laptop");
+}
+
+else if(money >= 10000)
+{
+    console.log("Cycle");
+}
+
+else
+{
+    console.log("Chocolate");
+}
